221a6b2d02ddf7d8c4f91ff969659d6e5df56aea
Seb Ashton <seb@madebymade.co.uk>
2013-10-31 14:48:05 +0000

    being thick

--- lib/turbo-sprockets/railtie.rb
+++ lib/turbo-sprockets/railtie.rb
@@ -25,7 +25,7 @@ module TurboSprockets
   end
 
   def self.get_source_manifest_path(manifest_dir, filename="sources_manifest.yml")
-    manifest_dir.join get_source_manifest_filename(filename)
+    "#{manifest_dir}/#{get_source_manifest_filename(filename)}"
   end
 
   def self.get_source_manifest_filename(filename="sources_manifest.yml")
